8bcfa3f4b3c44059ea948bd1a9c7a5b0cbe774a3
Eduardo BS <eduardobs17@outlook.com>
2019-01-22 19:18:17 -0600

    Slider de imagenes

--- app/src/main/java/com/example/applactancia/MainActivity.java
+++ app/src/main/java/com/example/applactancia/MainActivity.java
@@ -2,12 +2,60 @@ package com.example.applactancia;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.ViewSwitcher;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageSwitcher imageSwitcher;
+    private int[] galeria = {R.drawable.img1, R.drawable.img2, R.drawable.img3};
+    private int posicion;
+    private static final int DURACION = 5000;
+    private Timer timer = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageSwitcher = findViewById(R.id.imageSwitcher);
+        imageSwitcher.setFactory(new ViewSwitcher.ViewFactory() {
+            public View makeView() {
+                ImageView imageView = new ImageView(MainActivity.this);
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+
+                return imageView;
+            }
+        });
+
+        Animation fadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        Animation fadeOut = AnimationUtils.loadAnimation(this, R.anim.fade_out);
+        imageSwitcher.setInAnimation(fadeIn);
+        imageSwitcher.setOutAnimation(fadeOut);
+
+        startSlider();
+    }
+
+    public void startSlider() {
+        timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        imageSwitcher.setImageResource(galeria[posicion]);
+                        posicion++;
+                        if (posicion == galeria.length)
+                            posicion = 0;
+                    }
+                });
+            }
+        }, 0, DURACION);
     }
 }
