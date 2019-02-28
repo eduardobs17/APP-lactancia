663eed8c2220335cbc2cd44c4663c4a370dd1917
Eduardo BS <eduardobs17@outlook.com>
2019-02-27 23:12:40 -0600

    Agregadas pantallas de cuidados dentarios, paginas web y leyes

--- app/src/main/java/com/example/applactancia/MainActivity.java
+++ app/src/main/java/com/example/applactancia/MainActivity.java
@@ -18,7 +18,11 @@ import java.util.TimerTask;
 public class MainActivity extends AppCompatActivity {
 
     private ImageSwitcher imageSwitcher;
-    private int[] galeria = {R.drawable.img1, R.drawable.img2, R.drawable.img3};
+    private int[] galeria = {R.drawable.img1, R.drawable.img2, R.drawable.img3, R.drawable.img4, R.drawable.img5,
+            R.drawable.img6, R.drawable.img7, R.drawable.img8, R.drawable.img9, R.drawable.img10,
+            R.drawable.img11, R.drawable.img12, R.drawable.img13, R.drawable.img14, R.drawable.img15,
+            R.drawable.img16, R.drawable.img17, R.drawable.img18, R.drawable.img19, R.drawable.img20,
+            R.drawable.img21, R.drawable.img22, R.drawable.img23};
     private int posicion;
     private static final int DURACION = 5000;
     private Timer timer = null;
@@ -43,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
         imageSwitcher.setInAnimation(fadeIn);
         imageSwitcher.setOutAnimation(fadeOut);
 
-        //startSlider();
+        startSlider();
     }
 
     public void startSlider() {
