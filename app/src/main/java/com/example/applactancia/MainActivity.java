47396735baf1129293cedd915a126fcd3d4493c4
Eduardo BS <eduardobs17@outlook.com>
2019-02-27 23:29:06 -0600

    Agregadas pantallas de cuidados dentarios, paginas web y leyes

--- app/src/main/java/com/example/applactancia/MainActivity.java
+++ app/src/main/java/com/example/applactancia/MainActivity.java
@@ -6,64 +6,13 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.ImageSwitcher;
-import android.widget.ImageView;
-import android.widget.ViewSwitcher;
-
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ImageSwitcher imageSwitcher;
-    private int[] galeria = {R.drawable.img1, R.drawable.img2, R.drawable.img3, R.drawable.img4, R.drawable.img5,
-            R.drawable.img6, R.drawable.img7, R.drawable.img8, R.drawable.img9, R.drawable.img10,
-            R.drawable.img11, R.drawable.img12, R.drawable.img13, R.drawable.img14, R.drawable.img15,
-            R.drawable.img16, R.drawable.img17, R.drawable.img18, R.drawable.img19, R.drawable.img20,
-            R.drawable.img21, R.drawable.img22, R.drawable.img23};
-    private int posicion;
-    private static final int DURACION = 5000;
-    private Timer timer = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        imageSwitcher = findViewById(R.id.imageSwitcher);
-        imageSwitcher.setFactory(new ViewSwitcher.ViewFactory() {
-            public View makeView() {
-                ImageView imageView = new ImageView(MainActivity.this);
-                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-
-                return imageView;
-            }
-        });
-
-        Animation fadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in);
-        Animation fadeOut = AnimationUtils.loadAnimation(this, R.anim.fade_out);
-        imageSwitcher.setInAnimation(fadeIn);
-        imageSwitcher.setOutAnimation(fadeOut);
-
-        startSlider();
-    }
-
-    public void startSlider() {
-        timer = new Timer();
-        timer.scheduleAtFixedRate(new TimerTask() {
-            public void run() {
-                runOnUiThread(new Runnable() {
-                    public void run() {
-                        imageSwitcher.setImageResource(galeria[posicion]);
-                        posicion++;
-                        if (posicion == galeria.length)
-                            posicion = 0;
-                    }
-                });
-            }
-        }, 0, DURACION);
     }
 
     public void ejecutarInfo(View v) {
